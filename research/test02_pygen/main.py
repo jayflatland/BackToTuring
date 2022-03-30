7597caea45011bbb18c82d45ebfc99570d913ce3
Jay Flatland <jayflatland@gmail.com>
2022-03-30 18:56:15 -0500

    Actually parameterized assembly generation

--- research/test02_pygen/main.py
+++ research/test02_pygen/main.py
@@ -1,36 +1,51 @@
 #!/usr/bin/env python3
 
 
-example = r"""
-        .global _start
-        .text
-_start:
-        mov     $1, %rax
-        mov     $1, %rdi
-_loop:
-        mov     $message, %rsi
-        mov     $len, %rdx
-        syscall
-
-        mov     $60, %rax
-        xor     %rdi, %rdi
-        syscall
-
-        .data
-message:
-        .ascii  "Hello, world\n"
-        len = . - message
-"""
-
 class Generator:
     def __init__(self):
-        self.data = []
-        self.text = []
+        self.hdr = r"""
+            .global _start
+            .text
+            _start:
+        """
+        self.code = ""
+        self.exit = r"""
+                mov     $60, %rax
+                xor     %rdi, %rdi
+                syscall
+        """
+
+        self.data = r"""
+            .data
+        """
+        self.string_cnt = 1
 
     def output(self):
-        return example
+        return "".join([
+            self.hdr,
+            self.code,
+            self.exit,
+            self.data,
+        ])
 
+    def print(self, msg):
+        sid = f"_{self.string_cnt}"
+        self.string_cnt += 1
+        self.data += fr"""{sid}:
+            .ascii "{msg}\n"
+            {sid}_len = . - {sid}
+        """
+
+        self.code += fr"""
+            mov     $1, %rax
+            mov     $1, %rdi
+            mov     ${sid}, %rsi
+            mov     ${sid}_len, %rdx
+            syscall
+        """
 
 
 g = Generator()
+g.print("Hello world from python to assembly!")
+g.print("Ain't this just swell?")
 print(g.output())
